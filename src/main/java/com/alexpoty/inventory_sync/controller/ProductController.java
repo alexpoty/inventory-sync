c5735c6f5300eaa4636c664ab41d5c7b88b34589
Oleksandr <151620071+alexpoty@users.noreply.github.com>
2025-02-23 13:35:43 +0200

    Added controller Exception Handler

--- src/main/java/com/alexpoty/inventory_sync/controller/ProductController.java
+++ src/main/java/com/alexpoty/inventory_sync/controller/ProductController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -20,7 +17,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
+    @ResponseBody
     public ResponseEntity<ProductResponse> createProduct(@Valid  @RequestBody ProductRequest productRequest) {
-        return new ResponseEntity<>(productService.createProduct(productRequest), HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productRequest));
     }
 }
