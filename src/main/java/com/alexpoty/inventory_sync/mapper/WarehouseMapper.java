230c106afa2c79b079d26c58beb63eb55840d6ea
Oleksandr <151620071+alexpoty@users.noreply.github.com>
2025-02-24 22:52:18 +0200

    Create Dtos and Mapper for warehouse

---
+++ src/main/java/com/alexpoty/inventory_sync/mapper/WarehouseMapper.java
@@ -0,0 +1,16 @@
+package com.alexpoty.inventory_sync.mapper;
+
+import com.alexpoty.inventory_sync.dto.warehouse.WarehouseRequest;
+import com.alexpoty.inventory_sync.dto.warehouse.WarehouseResponse;
+import com.alexpoty.inventory_sync.model.Warehouse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface WarehouseMapper {
+
+    @Mapping(target = "products", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Warehouse toWarehouse(WarehouseRequest warehouseRequest);
+    WarehouseResponse toWarehouseResponse(Warehouse warehouse);
+}
