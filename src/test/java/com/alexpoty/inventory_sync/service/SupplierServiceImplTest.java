454428de6874ad8e75dd97b8b0f0ec5c119c3fd9
Oleksandr <151620071+alexpoty@users.noreply.github.com>
2025-02-27 19:04:36 +0200

    Finished writing unit tests

--- src/test/java/com/alexpoty/inventory_sync/service/SupplierServiceImplTest.java
+++ src/test/java/com/alexpoty/inventory_sync/service/SupplierServiceImplTest.java
@@ -2,6 +2,7 @@ package com.alexpoty.inventory_sync.service;
 
 import com.alexpoty.inventory_sync.dto.supplier.SupplierRequest;
 import com.alexpoty.inventory_sync.dto.supplier.SupplierResponse;
+import com.alexpoty.inventory_sync.exception.supplier.SupplierNotFoundException;
 import com.alexpoty.inventory_sync.model.Supplier;
 import com.alexpoty.inventory_sync.repository.SupplierRepository;
 import com.alexpoty.inventory_sync.service.impl.SupplierServiceImpl;
@@ -11,8 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -48,6 +51,63 @@ class SupplierServiceImplTest {
         assertEquals("Test", actual.contactInfo());
         verify(repository, times(1)).save(any(Supplier.class));
         verify(repository, times(1)).existsById(anyLong());
+    }
+
+    @Test
+    public void should_throw_when_updating_not_existing_supplier() {
+        // when
+        when(repository.existsById(anyLong())).thenReturn(false);
+        // assert
+        assertThrows(SupplierNotFoundException.class, () -> service.updateSupplier(1L, createSupplierRequest()));
+    }
+
+    @Test
+    public void should_get_a_list_of_suppliers() {
+        // when
+        when(repository.findAll()).thenReturn(List.of(createSupplier()));
+        List<SupplierResponse> actual = service.getAllSuppliers();
+        // assert
+        assertEquals(1, actual.size());
+        assertEquals("Test", actual.getFirst().name());
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    public void should_find_a_supplier_by_id() {
+        // when
+        when(repository.findById(anyLong())).thenReturn(Optional.of(createSupplier()));
+        SupplierResponse actual = service.getSupplier(1L);
+        // assert
+        assertNotNull(actual);
+        assertEquals("Test", actual.name());
+        assertEquals("Test", actual.contactInfo());
+        assertEquals(1L, actual.id());
+        verify(repository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    public void should_throw_SupplierNotFoundException() {
+        // when
+        when(repository.findById(anyLong())).thenReturn(Optional.empty());
+        // assert
+        assertThrows(SupplierNotFoundException.class, () -> service.getSupplier(1L));
+    }
+
+    @Test
+    public void should_delete_a_supplier() {
+        // when
+        doNothing().when(repository).deleteById(anyLong());
+        service.deleteSupplier(1L);
+        // assert
+        verify(repository, times(1)).deleteById(anyLong());
+    }
+
+    @Test
+    public void should_throw_when_deleting_supplier() {
+        // when
+        when(repository.existsById(anyLong())).thenReturn(false);
+        // assert
+        assertThrows(SupplierNotFoundException.class, () -> service.deleteSupplier(1L));
     }
 
     private Supplier createSupplier() {
