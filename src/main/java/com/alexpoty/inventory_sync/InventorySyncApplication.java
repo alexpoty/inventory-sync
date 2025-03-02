744f37d142f39bbd690dbc19006fac2ac1802d1c
Oleksandr <151620071+alexpoty@users.noreply.github.com>
2025-03-02 10:40:21 +0200

    Fixed bug for redis

--- src/main/java/com/alexpoty/inventory_sync/InventorySyncApplication.java
+++ src/main/java/com/alexpoty/inventory_sync/InventorySyncApplication.java
@@ -2,8 +2,14 @@ package com.alexpoty.inventory_sync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@EnableCaching
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class InventorySyncApplication {
 
 	public static void main(String[] args) {
